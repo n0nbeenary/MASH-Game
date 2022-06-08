8e8c56d780e22d29cf39cdf24c7c4e59c51e5d79
n0nbeenary <107054990+n0nbeenary@users.noreply.github.com>
2022-06-07 23:21:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // New function called random_number that takes one parameter, num (or a number)
-function random_number(num) 
-{
+function random_number(num) {
 	// Get a random number between 0 and a passed-in number
 	var num = num || 4  // If no number passed in, default to 4
 	return Math.floor(Math.random() * num); // Round the answer down (floor) of a random number between 0 and 1 and multiply it by a number. Then return a value and exit the function.
 }
 
 // New function called mash_choice that doesn't take any parameters 
-function mash_choice() 
-{
+function mash_choice() {
 	// Since MASH is a special case, give it its own list
 	var mash = ['mansion', 'apartment', 'shack', 'house'];  // The array of choices to pick from 
 	var randomNum = random_number(4);  // Use the above function to get a number between 0 and 4
@@ -16,27 +14,23 @@ function mash_choice()
 }
 
 // Get a random answer from the available answers in a given category
-function get_answer(category) 
-{ 
+function get_answer(category) { 
 	var choices = [];  // A blank array to hold the user provided answer  
 	var selector = 'input[name="' + category + '[]"]';  // Build a CSS selector for the blanks in our passed in category 
 	var inputs = document.querySelectorAll(selector);  // Get all of the inputs that match our selector 
 	var answer;
 
-	for (var i = 0; i < inputs.length; i++) 
-  {  // Begin a for loop that will run through the code. i++ = add one to the counter which is "i"
-		  answer = inputs[i].value;  // Get the input with the index value of the counter and get the value ie. if they typed in dog, you get back "dog" 
-		  if (answer !== '') 
-      {  // If answer doesn't equal a blank... !== means doesn't equal 
-			  choices.push(answer); //...add it to the end of the list 
-      }
-  }
+	for (var i = 0; i < inputs.length; i++) {  // Begin a for loop that will run through the code. i++ = add one to the counter which is "i"
+		answer = inputs[i].value;  // Get the input with the index value of the counter and get the value ie. if they typed in dog, you get back "dog" 
+		if (answer !== '') {  // If answer doesn't equal a blank... !== means doesn't equal 
+			choices.push(answer); //...add it to the end of the list 
+		}
+	}
 	return choices[random_number(choices.length)];   // Pick and return a random choice choice.length = number of answers the user provided in that category 
 }
 
 // Function to find the spans that need to be filled
-function fill_in_answers(answers) 
-{
+function fill_in_answers(answers) {
 	var home = document.querySelector('#home');  // This says make a new variable and find the HTML tag that has the ID of "home" 
 	var answer_2 = document.querySelector('#answer_2');
 	var answer_3 = document.querySelector('#answer_3');
@@ -54,8 +48,7 @@ function fill_in_answers(answers)
 }
 
 // Button submit handler function
-function handle_submission(evt) 
-{
+function handle_submission(evt) {
 	evt.preventDefault();  // Stop the form from reloading the page 
 	evt.stopPropagation();  // Stop the form from reloading the page
 	
